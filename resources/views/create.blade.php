19e6126f21931559ece6d0a4a9bf9c3bf507f8c4
Kévin Martin <hello@kmartin.io>
2018-08-14 12:18:23 +0200

    fixed #9

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -68,7 +68,7 @@
 
 @section('customJs')
     <!-- CDN clipboard.js -->
-    <script async src="{{ asset('js/clipboard.min.js') }}"></scriptasync>
+    <script src="{{ asset('js/clipboard.min.js') }}"></script>
     <script>
         $('button').tooltip({
             trigger: 'click',
